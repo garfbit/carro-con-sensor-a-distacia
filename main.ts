2d387a26825e95ac7cbb6dbab6499fb494e36510
garfbit <104676660+garfbit@users.noreply.github.com>
2023-08-26 00:02:15 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 let Distancia = 0
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P0, 1)
     Distancia = sonar.ping(
-    DigitalPin.P0,
     DigitalPin.P1,
+    DigitalPin.P2,
     PingUnit.Centimeters
     )
     basic.showNumber(Distancia)
-    basic.pause(1000)
     if (Distancia < 10) {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.No)
         pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        basic.showIcon(IconNames.Heart)
+        pins.digitalWritePin(DigitalPin.P0, 1)
     }
 })
